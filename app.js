ec1eae9516d662e15085d6ddd4dbc41c9f22dfde
Damien Mabin <dmabin@google.com>
2018-11-27 12:44:21 +0000

    WIP drawer & cards

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-import {MDCRipple} from '@material/ripple/index';
-const ripple = new MDCRipple(document.querySelector('.foo-button'));
+//import {MDCRipple} from '@material/ripple/index';
+//const ripple = new MDCRipple(document.querySelector('.foo-button'));
+
+import {MDCList} from "@material/list";
+const list = MDCList.attachTo(document.querySelector('.mdc-list'));
+list.wrapFocus = true;
